d6f6218cc244db533718d4bafec95b39c6a32022
Xcozy <xcozydegen@gmail.com>
2025-02-17 13:42:12 +0100

    CLI structure

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    // Block number to verify
     #[arg(short, long)]
     block: u64,
 }
