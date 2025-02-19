a0f78fb201ed867a4a1c0a6ef7fb848252a089f5
Xcozy <xcozydegen@gmail.com>
2025-02-19 01:22:23 +0100

    Provider setup, async config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
+use anyhow::Result;
 use clap::Parser;
+use ethers::providers::{Http, Provider};
+use std::sync::Arc;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
+    // Ethereum RPC URL
+    #[arg(short, long)]
+    rpc_url: String,
+
     // Block number to verify
     #[arg(short, long)]
     block: u64,
 }
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Args::parse();
+    let provider = Provider::<Http>::try_from(args.rpc_url)?;
+    let provider = Arc::new(provider);
+    
+    println!("Connected to Ethereum node!");
     println!("Will verify block: {}", args.block);
+    
+    Ok(())
 } 
