28463fd309228d7e2f9b898433bb4be66abec9c6
Xcozy <xcozydegen@gmail.com>
2025-03-07 01:34:33 +0100

    receipt processing

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use ethers::providers::{Http, Provider};
 use ethers::types::{H256, U64};
 use sha3::{Digest, Keccak256};
 use std::sync::Arc;
+use std::str::FromStr;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -14,6 +15,10 @@ struct Args {
     // Block number to verify
     #[arg(short, long)]
     block: u64,
+
+    // Transaction hash to verify
+    #[arg(short, long)]
+    tx_hash: String,
 }
 
 struct MerkleVerifier {
@@ -31,6 +36,14 @@ impl MerkleVerifier {
     async fn get_block_receipts_root(&self, block_number: U64) -> Result<H256> {
         let block = self.provider.get_block(block_number).await?.unwrap();
         Ok(block.receipts_root)
+    }
+
+    async fn get_receipt(&self, tx_hash: H256) -> Result<Vec<u8>> {
+        let receipt = self.provider.get_transaction_receipt(tx_hash).await?.unwrap();
+        
+        // In a real implementation, we would RLP encode the receipt here
+        // For now, we'll just use a placeholder hash
+        Ok(Keccak256::digest(&receipt.to_string()).to_vec())
     }
 
     fn verify_merkle_proof(&self, leaf: H256, proof: Vec<H256>, root: H256) -> bool {
@@ -65,10 +78,14 @@ async fn main() -> Result<()> {
 
     let verifier = MerkleVerifier::new(&rpc_url)?;
     let block_number = U64::from(args.block);
+    let tx_hash = H256::from_str(&args.tx_hash)?;
     
     let receipts_root = verifier.get_block_receipts_root(block_number).await?;
+    let receipt_data = verifier.get_receipt(tx_hash).await?;
+    
     println!("Connected to Ethereum node!");
     println!("Block {} receipts root: {:?}", args.block, receipts_root);
+    println!("Transaction receipt hash: {:?}", H256::from_slice(&receipt_data));
     
     Ok(())
 } 
